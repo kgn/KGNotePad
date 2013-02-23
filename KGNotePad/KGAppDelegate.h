b7f6ab5991caae172be27ede13c4d81072732029
David Keegan <git@davidkeegan.com>
2013-02-23 14:33:39 -0700

    Initial Commit

---
+++ KGNotePad/KGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGAppDelegate.h
+//  KGNotePad
+//
+//  Created by David Keegan on 2/23/13.
+//  Copyright (c) 2013 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KGNotePad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KGNotePad
+//
+//  Created by David Keegan on 2/23/13.
+//  Copyright (c) 2013 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KGAppDelegate class]));
+    }
+}
